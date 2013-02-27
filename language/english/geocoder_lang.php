5daf25a966f0e558c824fbbae2e3942e49bdcf53
kerastraight <john@kerastraight.co.uk>
2013-02-27 14:01:06 +0000

    Update language/english/geocoder_lang.php

--- language/english/geocoder_lang.php
+++ language/english/geocoder_lang.php
@@ -1,4 +1,4 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['streams.geocoder.name'] 				= 'Geocoder';
-$lang['streams.geocoder.geocoder_error']    = 'Geocode failed for the following reason';
+$lang['streams:geocoder.name'] 				= 'Geocoder';
+$lang['streams:geocoder.geocoder_error']    = 'Geocode failed for the following reason';
